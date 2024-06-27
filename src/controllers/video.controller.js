ec9b12913a8d5a3fda57da830cdd4c01163cf4b1
suchana06 <singhsuchana170@gmail.com>
2024-06-27 16:22:12 +0530

    adding controllers for delete video

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -153,7 +153,21 @@ const togglePublishStatus = asyncHandler(async (req, res) => {
     }
     return res.status(200).json(new ApiResponse(200, video, "published status updated successfully"));
 })
-
+const deleteVideo = asyncHandler(async (req, res) => {
+    const { videoId } = req.params
+    if (!videoId) {
+        throw new ApiError(400, "videoId is missing")
+    }
+    const video = await Video.findByIdAndDelete(videoId)
+    if (!video) {
+        throw new ApiError(404, "Video not found");
+    }
+    return res.status(200).json(new ApiResponse(200,{},"deleted the video"))
+})
+const getAllVideos = asyncHandler(async (req, res) => {
+    const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
+    //TODO: get all videos based on query, sort, pagination
+})
 
 
 export {
@@ -161,5 +175,7 @@ export {
     getVideoById,
     updateVideo,
     updateThumbnail,
-    togglePublishStatus
+    togglePublishStatus,
+    deleteVideo,
+    getAllVideos
 }
